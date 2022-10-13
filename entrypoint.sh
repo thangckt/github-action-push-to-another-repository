745315b508bbd1209260dcb36565dd0056263150
thangckt <46436648+thangckt@users.noreply.github.com>
2022-10-13 16:53:10 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -146,4 +146,4 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
-git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+git push -f "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
